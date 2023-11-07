e54877a79fce78865709cb737a6d17a8e2dd3581
Sung Gon Yi <skonmeme@gmail.com>
2023-11-07 18:31:14 +0900

    Kafka client update with argparse

--- src/kafka.py
+++ src/kafka.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
+import argparse
 import json
 import logging
+import time
 
 from confluent_kafka import Producer
 from confluent_kafka import Consumer
@@ -13,7 +15,7 @@ def delivery_report(err, msg):
     else:
         logging.devel('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-def produce(bootstrap_servers: str, topic: str):
+def produce(bootstrap_servers: str, topic: str, interval: float):
     p = Producer({'bootstrap.servers': bootstrap_servers})
 
     count = 0
@@ -21,8 +23,8 @@ def produce(bootstrap_servers: str, topic: str):
 		# Trigger any available delivery report callbacks from previous produce() calls
         p.poll(0)
 
-        time = datetime.now(timezone.utc)
-        msg = {'key': 'test', 'value': count, 'timestamp': time.isoformat()}
+        now = datetime.now(timezone.utc)
+        msg = {'key': 'test', 'value': count, 'timestamp': now.isoformat()}
 
         # Asynchronously produce a message. The delivery report callback will
         # be triggered from the call to poll() above, or flush() below, when the
@@ -30,6 +32,7 @@ def produce(bootstrap_servers: str, topic: str):
         p.produce(topic, json.dumps(msg).encode('utf-8'), callback=delivery_report)
 
         count += 1
+        time.sleep(interval)
     
     # Wait for any outstanding messages to be delivered and delivery report
     # callbacks to be triggered.
@@ -57,3 +60,27 @@ def consume(bootstrap_servers: str, group_id: str, topic: str):
     
     # If comsuming is stopped, close the consumer
     c.close()
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Kafka clients')
+    parser.add_argument('-b', '--bootstrap-servers', type=str, required=True, help='Kafka bootstrap servers')
+    parser.add_argument('-t', '--topic', type=str, required=True, help='Kafka topic')
+    parser.add_argument('-g', '--group-id', type=str, required=False, help='Kafka consumer group id')
+    parser.add_argument('-v', '--verbose', action='count', required=False, default=0, help='Verbose logging')
+    parser.add_argument('-i', '--interval', type=float, required=False, default=1.0, help='Interval between messages')
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument('-p', '--produce', action='store_true', help='Produce messages')
+    mode.add_argument('-c', '--consume', action='store_true', help='Consume messages')
+    args = parser.parse_args()
+
+    if args.verbose == 1:
+        logging.basicConfig(level=logging.INFO)
+    elif args.verbose > 1:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+    
+    if args.produce:
+        produce(args.bootstrap_servers, args.topic, args.interval)
+    elif args.consume:
+        consume(args.bootstrap_servers, args.group_id, args.topic)
